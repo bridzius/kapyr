83211b7ddd5445d008acb14dac6448c54bfe838f
Jurgis Bridzius <jurgis@bridzius.lt>
2022-09-09 22:42:45 +0300

    Calling each place for details

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use reqwest;
 use std::error::Error;
 use std::time::Duration;
 use serde::{Deserialize, Serialize};
+use serde_json::{Value};
+use std::thread::sleep;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -17,7 +19,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let places: Vec<Place> = serde_json::from_str(&places_json)?;
 
     for x in places {
-    println!("{:?}", x);
+        let one_sec = Duration::from_secs(1);
+        //TODO: Remove this
+        sleep(one_sec);
+        println!("Calling {}", x.code);
+        let res = call_place(x.code).await?;
+        println!("{}", res);
     }
     Ok(())
 }
@@ -29,5 +36,18 @@ struct Place {
     name: String,
     administrativeDivision: String,
     countryCode: String
+}
+
+async fn call_place(code: String) -> Result<Value, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+
+    let place_uri = String::from("https://api.meteo.lt/v1/places/") + &code; 
+
+    let place_json = client.get(place_uri)
+        .header("Accept", "application/json")
+        .send().await?.text().await?;
+
+    let place: Value = serde_json::from_str(&place_json)?;
+    Ok(place)
 }
 
